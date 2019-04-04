06f31da00cb8539b6c98043adf632723f82b6969
Zac Garby <me@zacgarby.co.uk>
2019-04-04 10:13:36 +0100

    Start making the repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Control.Monad
 import Parse
 
 main :: IO ()
-main = return ()
+main = repl
+
+repl :: IO ()
+repl = forever $ do
+    putStr "zzzz> "
+    line <- getLine
+    case parse line of
+        Nothing -> putStrLn "no parse"
+        Just p -> print p
