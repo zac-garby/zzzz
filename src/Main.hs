9819a754333692a54c88b10e81e4f0dfede9b459
Zac Garby <me@zacgarby.co.uk>
2019-04-17 17:56:08 +0100

    Add the 'if' builtin function

--- src/Main.hs
+++ src/Main.hs
@@ -13,11 +13,17 @@ prelude = Env $ M.fromList
     [ ("x",      parse' "5")
     , ("double", parse' "(lambda (x) (+ x x))")
     , ("pair",   parse' "(lambda (x) '(x x))")
-    , ("+",      Builtin [Strict, Strict] add) ]
+    , ("+",      Builtin [Strict, Strict] add)
+    , ("if",     Builtin [Strict, Lazy, Lazy] ifFn) ]
 
 add :: [Expr] -> Result
 add [Number x, Number y] = ok $ Number (x + y)
 add _ = err "only numbers can be added using +"
+
+ifFn :: [Expr] -> Result
+ifFn [(List [(Symbol "quote"), Symbol "true"]), a, _] = ok a
+ifFn [(List [(Symbol "quote"), Symbol "false"]), _, b] = ok b
+ifFn _ = err "an if-expression should be in the form (if cond a b)"
 
 main :: IO ()
 main = repl
