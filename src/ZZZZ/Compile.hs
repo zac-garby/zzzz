60fc96fb6f319e1baf87e7aac4a37ee385239fa3
Zac Garby <me@zacgarby.co.uk>
2019-04-28 13:46:05 +0100

    Finish off the compiler... I think?

--- src/ZZZZ/Compile.hs
+++ src/ZZZZ/Compile.hs
@@ -9,12 +9,15 @@ import ZZZZ.Data
 
 -- | Compiles an expression (which has probably just been parsed) into
 -- | a term for evaluation.
-compile :: Expr -> Term
+compile :: Expr -> Either Error Term
 
-compile (ExSym s) = Symbol s
-compile (ExNum n) = Number n
-compile (ExStr s) = mkList . map Character $ s
-compile (ExChar c) = Character c
+compile (ExSym s) = Right $ Symbol s
+compile (ExNum n) = Right $ Number n
+compile (ExStr s) = Right $ mkList . map Character $ s
+compile (ExChar c) = Right $ Character c
+compile (ExList []) = Left "an empty list isn't allowed"
+compile (ExList (x:xs)) = apply <$> compile x <*> traverse compile xs
+compile (ExArr xs) = mkList <$> traverse compile xs
 
 -- | Utilises currying to apply a lambda abstraction to multiple arguments.
 apply :: Term -> [Term] -> Term
