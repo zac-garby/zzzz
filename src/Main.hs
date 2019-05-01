0867466e67b410b6c366bac9c6f34d68c7095292
Zac Garby <me@zacgarby.co.uk>
2019-05-01 10:27:06 +0100

    Make a REPL

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import System.IO
+import Control.Monad
+
+import ZZZZ.Compile
+
 main :: IO ()
-main = return ()
+main = repl
+
+repl = forever rep
+
+rep :: IO ()
+rep = do
+    putStr "zzzz> "
+    hFlush stdout
+    inp <- getLine
+    case compileString inp of
+        Left err -> putStrLn $ "error: " ++ err
+        Right term -> print term
