0bfe0174817d0dd706ca29cb44118a01f3cfcc87
Zac Garby <me@zacgarby.co.uk>
2019-05-09 19:00:42 +0100

    Make evaluation work
    I made a function to reduce terms to weak-head normal form, which is then used to reduce the functions inside an application of a non-beta-normal-term to some argument, to allow the application to be evaluated in the future.

--- src/ZZZZ/Eval.hs
+++ src/ZZZZ/Eval.hs
@@ -1,5 +1,7 @@
 module ZZZZ.Eval
     ( reduce
+    , whnf
+    , isNormal
     ) where
 
 import qualified Control.Monad.State as S
@@ -16,10 +18,26 @@ reduce (Symbol x n) = do
         Nothing -> S.lift . Left $ "undefined symbol: " ++ x
 
 reduce (Application (Abstraction (Symbol p n) b) x) = return $ sub p n x b
-reduce (Application f _) = S.lift . Left $ "invalid application. only functions can be applied, and the parameter must be a symbol. got function: " ++ show f
+reduce (Application f x)
+    | isNormal f = S.lift . Left $ "invalid application. only functions can be applied, and the parameter must be a symbol. got function: " ++ show f
+    | otherwise = do
+        env <- S.get
+        reduced <- S.lift $ whnf env f
+        return $ Application reduced x
 
 -- Things already in normal form
 reduce x = return x
+
+-- | Checks whether or not a lambda term is in β normal form.
+isNormal :: Term -> Bool
+isNormal (Application _ _) = False
+isNormal _ = True
+
+-- | Reduces a term to weak-head-normal-form by repeated β-reduction, with
+-- | respect to the given environment.
+whnf :: Env -> Term -> Result Term
+whnf env t | isNormal t = return t
+           | otherwise = S.evalStateT (reduce t) env >>= whnf env
 
 -- TODO: It's pretty stupid to
 -- have abstraction parameters as generic 'Term's. There should be
