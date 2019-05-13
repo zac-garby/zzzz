e6ad8b84c9fcf4dde65000bcf85a7eafea814b05
Zac Garby <me@zacgarby.co.uk>
2019-05-13 09:55:27 +0100

    Define some builtin functions

--- src/ZZZZ/Builtin.hs
+++ src/ZZZZ/Builtin.hs
@@ -1,3 +1,24 @@
 module ZZZZ.Builtin
-    (
+    ( builtins
     ) where
+
+import ZZZZ.Data
+
+-- In builtin functions, it can be assumed that the correct amount
+-- of elements are inside the argument list. Variadic functions are not
+-- possible in lambda calculus and so they are not possible in builtin
+-- functions either.
+
+-- | A list containing all available builtin functions.
+builtins :: [(String, [Term] -> Result Term)]
+builtins =
+    [ ("+", addB)
+    , ("-", subB) ]
+
+addB :: [Term] -> Result Term
+addB [Number a, Number b] = return $ Number (a + b)
+addB _ = Left "+ can only be applied to two numbers"
+
+subB :: [Term] -> Result Term
+subB [Number a, Number b] = return $ Number (a - b)
+subB _ = Left "- can only be applied to two numbers"
