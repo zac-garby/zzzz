d8cf73f32601d5c0fec79fb86a314049f84a55d1
Zac Garby <me@zacgarby.co.uk>
2019-05-13 10:04:28 +0100

    Pass an environment through the REPL

--- src/Main.hs
+++ src/Main.hs
@@ -5,19 +5,24 @@ import Control.Monad
 
 import ZZZZ.Compile
 import ZZZZ.Eval
+import ZZZZ.Data
 
 main :: IO ()
-main = repl
+main = void $ repl mempty
 
 -- | Repeats REPL iterations infinitely.
-repl = forever rep
+repl :: Env -> IO Env
+repl env = do
+    env' <- rep env
+    repl env'
 
 -- | A single REPL iteration. Takes an input, evaluates it, and prints out the result.
-rep :: IO ()
-rep = do
+rep :: Env -> IO Env
+rep env = do
     putStr "λ> "
     hFlush stdout
     inp <- getLine
-    case compileString inp >>= whnf mempty of
+    case compileString inp >>= whnf env of
         Left err -> putStrLn $ "error: " ++ err
         Right term -> print term
+    return env
